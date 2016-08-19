f27201164a80d01acde60c0ca7e94dd059be221b
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2016-08-19 21:19:40 +0430

    update readme and versions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PiCompress
+
 Compress and optimize JPEG and PNG images by using Tinify API of TinyPNG.com
+
+Application Main From:
+
+![MainForm](https://raw.githubusercontent.com/Behzadkhosravifar/PiCompress/master/img/MainForm.png)
+
+You can get your self Tinify API key from [here](https://tinypng.com/developers) and add keys in `PiCompress.exe.config` file to using from app.
+
+You can see all keys ramain count in Key manager form:
+
+![KeyManager](https://raw.githubusercontent.com/Behzadkhosravifar/PiCompress/master/img/KeyManager.png)
--- src/PiCompress/KeyManager.cs
+++ src/PiCompress/KeyManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Threading;
 using System.Windows.Forms;
 using PiCompress.Properties;
 
@@ -7,6 +8,8 @@ namespace PiCompress
 {
     public partial class KeyManager : Form
     {
+        private readonly CancellationTokenSource _cts;
+
         public KeyManager()
         {
             InitializeComponent();
@@ -16,6 +19,8 @@ namespace PiCompress
             colKeyName.HeaderText = Localization.Name;
             colKeyValue.HeaderText = Localization.API_Key;
             colKeyRemainCount.HeaderText = Localization.CompressRemainCount;
+
+            _cts = new CancellationTokenSource();
         }
 
         private async void KeyManager_Load(object sender, EventArgs e)
@@ -25,8 +30,16 @@ namespace PiCompress
             {
                 var tinifyKey = prop.DefaultValue.ToString();
                 var count = await TinifyImage.CompressRemainCountAsync(tinifyKey);
+                if(_cts.IsCancellationRequested) return;
                 dgvKeys.Rows.Add(row++, prop.Name, tinifyKey, TinifyImage.MaxCompressCount - count);
             }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            _cts.Cancel();
         }
     }
 }
